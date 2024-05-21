7249c54e8aa357cceb949f1dce255fa0839eeac6
Muthoni <cynthiamuthoni3@gmail.com>
2024-05-21 10:04:50 +0300

    updated request mapping for excel data immportation

--- Server/ticket_service/src/main/java/com/emtechhouse/Ticket/imp/ImpController.java
+++ Server/ticket_service/src/main/java/com/emtechhouse/Ticket/imp/ImpController.java
@@ -3,15 +3,15 @@ package com.emtechhouse.Ticket.imp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api/v1/trialbalance")
 public class ImpController {
 
 
